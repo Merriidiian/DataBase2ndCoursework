7b06e108aae5459cc394e0075511148643b57047
Merriidiian <iluxa9695@mail.ru>
2022-05-24 16:10:35 +0300

    + searchCook

--- src/BaseDataProject/searchOrders.java
+++ src/BaseDataProject/searchOrders.java
@@ -1,12 +1,21 @@
 package BaseDataProject;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import static BaseDataProject.Main.*;
+import static BaseDataProject.Main.connection;
 
 public class searchOrders {
 
@@ -33,8 +42,41 @@ public class searchOrders {
 
     @FXML
     void initialize() {
+        searchButton.setOnAction(event -> {
+            try {
+                searchOrdersMethod();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        exitButton.setOnAction(event -> {
+            exitToAdmin();
+        });
     }
-
-
+    void searchOrdersMethod () throws SQLException {
+        String textOutInOrder = "";
+        typeProjects = connection.prepareStatement("SELECT * FROM rest.list_orders WHERE (sum > '"+ Integer.parseInt(searchOrders.getText()) +"');");
+        tp = typeProjects.executeQuery();
+        sendingCommands = connection.createStatement();
+        while (tp.next()){
+            textOutInOrder = textOutInOrder + "\n" + "Order №" + tp.getString("id") + " was made in " + tp.getString("time_begin") + "-" + tp.getString("time_end") + ". Sum order = " + tp.getString("sum") + "$";
+        }
+        searchOrdersText.setText(textOutInOrder);
+    }
+    public void exitToAdmin() {
+        exitButton.getScene().getWindow().hide();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/BaseDataProject/admin.fxml"));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Админ");
+        stage.show();
+    }
 }
 
